0e5e9bbeadfff73975cf28a1790f6793610540b8
João Vareiro <joao.vareiro@ufms.br>
2023-06-21 08:50:49 -0400

    feat: Update java classes

--- app/src/main/java/com/example/tier_list_app/model/Item.java
+++ app/src/main/java/com/example/tier_list_app/model/Item.java
@@ -3,9 +3,15 @@ package com.example.tier_list_app.model;
 import java.io.Serializable;
 
 public class Item implements Serializable {
+
+    private String tierName;
+
     private String name;
-    private String tier;
     private String urlItem;
+
+    int id;
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
     public String getName() {
         return name;
@@ -15,12 +21,12 @@ public class Item implements Serializable {
         this.name = name;
     }
 
-    public String getTier() {
-        return tier;
+    public String getTierName() {
+        return tierName;
     }
 
-    public void setTier(String tier) {
-        this.tier = tier;
+    public void setTierName(String tier) {
+        this.tierName = tier;
     }
 
     public String getUrlItem() {
